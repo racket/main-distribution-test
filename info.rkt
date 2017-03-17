413094ed57ad4af858daa51201373dc6554fb960
Jay McCarthy <jay.mccarthy@gmail.com>
2017-03-17 09:51:51 -0400

    Add net-cookies because of web-server

--- info.rkt
+++ info.rkt
@@ -24,6 +24,7 @@
                "compatibility-test"
                "data-test"
                "net-test"
+               "net-cookies-test"
                "pconvert-test"
                "planet-test"
                "syntax-color-test"
